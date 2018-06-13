54c8f5025932ba2bb1eb5e2b24b03234cefafcb1
duartello <compilingrom@gmail.com>
2018-06-13 11:37:38 +0000

    Initial Xtended Commit

--- msm8996.mk
+++ msm8996.mk
@@ -206,7 +206,7 @@ PRODUCT_PACKAGES += \
 
 # Doze mode
 PRODUCT_PACKAGES += \
-    Doze
+    OneplusThreeDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -275,11 +275,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm \
-    vendor.lineage.livedisplay-V1.0-java
-
-PRODUCT_BOOT_JARS += \
-    vendor.lineage.livedisplay-V1.0-java
+    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
